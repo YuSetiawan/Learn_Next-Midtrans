6ce445f35301c0de93da4d7a10664d39ca22dcf7
YuSetiawan <ysfstiawan@gmail.com>
2023-12-26 14:43:43 +0700

    Midtrans Snap Payment

---
+++ app/libs/product.js
@@ -0,0 +1,13 @@
+export const product = {
+  id: ~~(Math.random() * 100) + 1,
+  image: '/img.png',
+  name: 'Nike SuperRep Go',
+  price: 890000,
+  description: 'The Nike Superrep Go Combines Comfortable Foam Cushioning, Flexibility And Support To Get You Moving In Circuit-based Fitness Classes Or While Streaming Workouts At Home.',
+  colors: [
+    {value: 'putih', label: 'Putih'},
+    {value: 'biru', label: 'Biru'},
+    {value: 'coklat', label: 'Coklat'},
+    {value: 'kuning', label: 'Kuning'},
+  ],
+};
